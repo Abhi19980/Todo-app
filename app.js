dabe21dffbc757b33a534d2d31f0de2c1d845c1e
Abhishek Gupta <abhishekgupta1498@gmail.com>
2020-12-14 13:49:28 +0530

    input

---
+++ app.js
@@ -0,0 +1,34 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-input');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+
+function andTodo(event) { 
+    //prevent form from submitting
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = "hey";
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.innerText = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //CHECK trash BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.innerText = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
+}
