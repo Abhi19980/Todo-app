121d74f4c247814f7fc57bae6f28cc93a9dd59df
Abhishek Gupta <abhishekgupta1498@gmail.com>
2020-12-15 12:30:10 +0530

    work

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Selectors
 const todoInput = document.querySelector(".todo-input");
-const todoButton = document.querySelector(".todo-Button");
+const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
