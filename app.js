d793ff374e0e57797ebd2e0f2329be5414584ca6
Abhishek Gupta <abhishekgupta1498@gmail.com>
2020-12-20 00:05:32 +0530

    tic

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
-todoList.addEventListener('click', deleteCheck);
+todoList.addEventListener("click", deleteCheck);
 
 //Functions
 
@@ -54,5 +55,7 @@ function deleteCheck(e) {
     todo.classList.toggle("completed");
   }
 }
-
-function
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  console.log(todos);
+}
